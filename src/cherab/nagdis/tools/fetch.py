528ecca544c0bcbdfcff7a9f4601794df86fbe97
munechika-koyo <munechika.koyo@gmail.com>
2025-01-31 08:22:02 +0100

    ⚡️ Enhance SFTP file fetching with default parameters and improved registry display

--- src/cherab/nagdis/tools/fetch.py
+++ src/cherab/nagdis/tools/fetch.py
@@ -1,16 +1,20 @@
 """Module to fetch files from the remote server using the SFTP downloader."""
 
+from __future__ import annotations
+
 import os
 
 import pooch
 from pooch import SFTPDownloader
+from rich.console import Console
+from rich.table import Table
 
 __all__ = ["fetch_file", "show_registries"]
 
 
-HOSTNAME = os.environ.get("SSH_RAYTRACE_HOSTNAME")
-USERNAME = os.environ.get("SSH_RAYTRACE_USERNAME")
-PASSWORD = os.environ.get("SSH_RAYTRACE_PASSWORD")
+HOSTNAME = os.environ.get("SSH_RAYTRACE_HOSTNAME", default="sftp://example.com/")
+USERNAME = os.environ.get("SSH_RAYTRACE_USERNAME", default="username")
+PASSWORD = os.environ.get("SSH_RAYTRACE_PASSWORD", default="password")
 
 # Registry of the datasets
 REGISTRIES = {
@@ -33,15 +37,21 @@ REGISTRIES = {
 
 def show_registries() -> None:
     """Show the registries of the datasets."""
-    for key, value in REGISTRIES.items():
-        print(f"{key}: {value}")
+    table = Table(title="Registries", show_lines=True)
+    table.add_column("File Name", style="cyan", justify="left")
+    table.add_column("SHA256", style="dim", justify="left")
+
+    for name, sha256 in REGISTRIES.items():
+        table.add_row(name, sha256)
+    console = Console()
+    console.print(table)
 
 
 def fetch_file(
     name: str,
-    host: str | None = HOSTNAME,
-    username: str | None = USERNAME,
-    password: str | None = PASSWORD,
+    host: str = HOSTNAME,
+    username: str = USERNAME,
+    password: str = PASSWORD,
 ) -> str:
     """Fetch the file from the remote server using the configured SFTP downloader.
 
@@ -52,28 +62,21 @@ def fetch_file(
     name : str
         Name of the file to fetch.
     host : str, optional
-        Host name of the server, by default None.
-        If None, it will use the value from the environment variable `SSH_RAYTRACE_HOSTNAME`.
-        host name should be in the format `sftp://example.com/{directories}`.
+        Host name of the server, by default ``sftp://example.com/``.
+        This value is adaptable from the environment variable `SSH_RAYTRACE_HOSTNAME`.
+        Host name should be in the format ``sftp://{host's name or ip}/{directories}``.
     username : str, optional
-        Username to authenticate with the server, by default None.
-        If None, it will use the value from the environment variable `SSH_RAYTRACE_USERNAME`.
+        Username to authenticate with the server, by default ``username``.
+        This value is adaptable from the environment variable `SSH_RAYTRACE_USERNAME`.
     password : str, optional
-        Password to authenticate with the server, by default None.
-        If None, it will use the value from the environment variable `SSH_RAYTRACE_PASSWORD`.
+        Password to authenticate with the server, by default ``password``.
+        This value is adaptable from the environment variable `SSH_RAYTRACE_PASSWORD`.
 
     Returns
     -------
     str
         Path to the fetched file.
     """
-    if host is None:
-        raise ValueError("Please provide a valid host name like sftp://example.com/directories.")
-    if username is None:
-        raise ValueError("Please provide a valid username for the server.")
-    if password is None:
-        raise ValueError("Please provide a valid password for the server.")
-
     pup = pooch.create(
         path=pooch.os_cache("cherab/nagdis"),
         base_url=host,
@@ -84,5 +87,6 @@ def fetch_file(
         username=username,
         password=password,
         progressbar=True,
+        timeout=5,
     )
     return pup.fetch(name, downloader=downloader)
